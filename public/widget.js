2e8628471175ac516c19c6d8537f24d74daec5ea
Hammad <142727701+Just-Hammad@users.noreply.github.com>
2024-08-08 15:58:10 +0500

    fixing widget

--- public/widget.js
+++ public/widget.js
@@ -1,20 +1,19 @@
 // public/widget.js
 (function() {
-    var script = document.createElement('script');
-    script.src = 'https://your-domain.com/widget'; // URL of the widget page
-    script.async = true;
-    script.onload = function() {
-      var iframe = document.createElement('iframe');
-      iframe.src = 'https://your-domain.com/widget';
-      iframe.width = '300';
-      iframe.height = '400';
-      iframe.style.border = 'none';
-      iframe.style.position = 'fixed';
-      iframe.style.bottom = '0';
-      iframe.style.right = '0';
-      iframe.style.zIndex = '9999';
-      document.body.appendChild(iframe);
-    };
-    document.head.appendChild(script);
-  })();
-  
+  var script = document.createElement('script');
+  script.src = 'http://167.71.205.129/widget'; // Updated URL of the widget page
+  script.async = true;
+  script.onload = function() {
+    var iframe = document.createElement('iframe');
+    iframe.src = 'http://167.71.205.129/widget'; // Updated URL of the widget page
+    iframe.width = '300';
+    iframe.height = '400';
+    iframe.style.border = 'none';
+    iframe.style.position = 'fixed';
+    iframe.style.bottom = '0';
+    iframe.style.right = '0';
+    iframe.style.zIndex = '9999';
+    document.body.appendChild(iframe);
+  };
+  document.head.appendChild(script);
+})();
